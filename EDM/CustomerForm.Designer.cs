fc07783592016e4ebeec6e74352add6b2381708b
skyxuan <syncshinee@gmail.com>
2015-12-26 14:15:35 +0800

    cunstomer form version2

--- EDM/CustomerForm.Designer.cs
+++ EDM/CustomerForm.Designer.cs
@@ -30,8 +30,6 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -39,25 +37,49 @@
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Column_goodsname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column_goodstype = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column_goodswight = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.buttonUpdKey = new System.Windows.Forms.Button();
+            this.infoUpdate = new System.Windows.Forms.Button();
+            this.textBoxMail = new System.Windows.Forms.TextBox();
+            this.textBoxPhn = new System.Windows.Forms.TextBox();
+            this.textBoxAge = new System.Windows.Forms.TextBox();
+            this.textBoxSex = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxAccount = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
-            this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.tabPage3.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(-6, -2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(-6, -1);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(718, 400);
+            this.tabControl1.Size = new System.Drawing.Size(718, 399);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -72,33 +94,9 @@
             this.tabPage1.Text = "我的订单";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
-            // 
-            this.tabPage2.Controls.Add(this.button2);
-            this.tabPage2.Controls.Add(this.label2);
-            this.tabPage2.Controls.Add(this.label1);
-            this.tabPage2.Controls.Add(this.textBox2);
-            this.tabPage2.Controls.Add(this.textBox1);
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(710, 374);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "提交订单";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(14, 6);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(66, 30);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "查询";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // dataGridView1
             // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
@@ -144,46 +142,241 @@
             this.Column6.HeaderText = "下单时间";
             this.Column6.Name = "Column6";
             // 
-            // textBox1
+            // button1
             // 
-            this.textBox1.Location = new System.Drawing.Point(29, 54);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(137, 21);
-            this.textBox1.TabIndex = 0;
+            this.button1.Location = new System.Drawing.Point(14, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "查询";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox2
+            // tabPage2
             // 
-            this.textBox2.Location = new System.Drawing.Point(29, 138);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(136, 21);
-            this.textBox2.TabIndex = 1;
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(710, 373);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "提交订单";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // dataGridView2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 29);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 12);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "发货地址";
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column_goodsname,
+            this.Column_goodstype,
+            this.Column_goodswight});
+            this.dataGridView2.Location = new System.Drawing.Point(62, 69);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 23;
+            this.dataGridView2.Size = new System.Drawing.Size(343, 161);
+            this.dataGridView2.TabIndex = 5;
             // 
-            // label2
+            // Column_goodsname
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(27, 107);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 12);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "收货地址";
+            this.Column_goodsname.HeaderText = "货物名称";
+            this.Column_goodsname.Name = "Column_goodsname";
+            // 
+            // Column_goodstype
+            // 
+            this.Column_goodstype.HeaderText = "货物类型";
+            this.Column_goodstype.Name = "Column_goodstype";
+            // 
+            // Column_goodswight
+            // 
+            this.Column_goodswight.HeaderText = "货物重量";
+            this.Column_goodswight.Name = "Column_goodswight";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(29, 203);
+            this.button2.Location = new System.Drawing.Point(312, 272);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(69, 27);
             this.button2.TabIndex = 4;
             this.button2.Text = "提交订单";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(474, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "收货地址";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(474, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "发货地址";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(476, 197);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(136, 21);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(476, 101);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(137, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.buttonUpdKey);
+            this.tabPage3.Controls.Add(this.infoUpdate);
+            this.tabPage3.Controls.Add(this.textBoxMail);
+            this.tabPage3.Controls.Add(this.textBoxPhn);
+            this.tabPage3.Controls.Add(this.textBoxAge);
+            this.tabPage3.Controls.Add(this.textBoxSex);
+            this.tabPage3.Controls.Add(this.textBoxName);
+            this.tabPage3.Controls.Add(this.textBoxAccount);
+            this.tabPage3.Controls.Add(this.label8);
+            this.tabPage3.Controls.Add(this.label7);
+            this.tabPage3.Controls.Add(this.label6);
+            this.tabPage3.Controls.Add(this.label5);
+            this.tabPage3.Controls.Add(this.label4);
+            this.tabPage3.Controls.Add(this.label3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(710, 373);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "个人信息";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // buttonUpdKey
+            // 
+            this.buttonUpdKey.Location = new System.Drawing.Point(394, 213);
+            this.buttonUpdKey.Name = "buttonUpdKey";
+            this.buttonUpdKey.Size = new System.Drawing.Size(91, 44);
+            this.buttonUpdKey.TabIndex = 13;
+            this.buttonUpdKey.Text = "修改密码";
+            this.buttonUpdKey.UseVisualStyleBackColor = true;
+            // 
+            // infoUpdate
+            // 
+            this.infoUpdate.Location = new System.Drawing.Point(198, 213);
+            this.infoUpdate.Name = "infoUpdate";
+            this.infoUpdate.Size = new System.Drawing.Size(91, 44);
+            this.infoUpdate.TabIndex = 12;
+            this.infoUpdate.Text = "更新个人信息";
+            this.infoUpdate.UseVisualStyleBackColor = true;
+            // 
+            // textBoxMail
+            // 
+            this.textBoxMail.Location = new System.Drawing.Point(428, 121);
+            this.textBoxMail.Name = "textBoxMail";
+            this.textBoxMail.Size = new System.Drawing.Size(113, 21);
+            this.textBoxMail.TabIndex = 11;
+            // 
+            // textBoxPhn
+            // 
+            this.textBoxPhn.Location = new System.Drawing.Point(428, 77);
+            this.textBoxPhn.Name = "textBoxPhn";
+            this.textBoxPhn.Size = new System.Drawing.Size(113, 21);
+            this.textBoxPhn.TabIndex = 10;
+            // 
+            // textBoxAge
+            // 
+            this.textBoxAge.Location = new System.Drawing.Point(428, 33);
+            this.textBoxAge.Name = "textBoxAge";
+            this.textBoxAge.Size = new System.Drawing.Size(113, 21);
+            this.textBoxAge.TabIndex = 9;
+            // 
+            // textBoxSex
+            // 
+            this.textBoxSex.Location = new System.Drawing.Point(184, 121);
+            this.textBoxSex.Name = "textBoxSex";
+            this.textBoxSex.Size = new System.Drawing.Size(100, 21);
+            this.textBoxSex.TabIndex = 8;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(184, 77);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 21);
+            this.textBoxName.TabIndex = 7;
+            // 
+            // textBoxAccount
+            // 
+            this.textBoxAccount.Location = new System.Drawing.Point(184, 33);
+            this.textBoxAccount.Name = "textBoxAccount";
+            this.textBoxAccount.Size = new System.Drawing.Size(100, 21);
+            this.textBoxAccount.TabIndex = 6;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(361, 124);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(29, 12);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "邮箱";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(361, 80);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 12);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "手机";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(361, 36);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 12);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "年龄";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(115, 124);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 12);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "性别";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(115, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "姓名";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(115, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "用户名";
             // 
             // CustomerForm
             // 
@@ -193,11 +386,15 @@
             this.Controls.Add(this.tabControl1);
             this.Name = "CustomerForm";
             this.Text = "CustomerForm";
+            this.Load += new System.EventHandler(this.CustomerForm_Load);
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.tabPage2.ResumeLayout(false);
             this.tabPage2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -220,5 +417,24 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column_goodsname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column_goodstype;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column_goodswight;
+        private System.Windows.Forms.Button buttonUpdKey;
+        private System.Windows.Forms.Button infoUpdate;
+        private System.Windows.Forms.TextBox textBoxMail;
+        private System.Windows.Forms.TextBox textBoxPhn;
+        private System.Windows.Forms.TextBox textBoxAge;
+        private System.Windows.Forms.TextBox textBoxSex;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxAccount;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
     }
 }
--- EDM/CustomerForm.cs
+++ EDM/CustomerForm.cs
@@ -14,16 +14,20 @@ namespace EDM
 {
     public partial class CustomerForm : Form
     {
+
+        private Dictionary<String, String> mp;
+
         public CustomerForm()
         {
+            mp = new Dictionary<String, String>();
+            mp.Clear();
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             MySqlCommand mComd;
-            mComd = new MySqlCommand("select * from order where userid='" + ManageForm.userid + "';", ManageForm.mConn);
-            dataGridView1.ReadOnly = true;
+            mComd = new MySqlCommand("select * from expressdata.order where userid='" + ManageForm.userid + "';", ManageForm.mConn);
             MySqlDataAdapter da = new MySqlDataAdapter(mComd);
             DataSet ds = new DataSet();
             da.Fill(ds, "order");
@@ -38,6 +42,59 @@ namespace EDM
             //todo
             mComd = new MySqlCommand("select * from order, ", ManageForm.mConn);
 
+        }
+
+        private void CustomerForm_Load(object sender, EventArgs e)
+        {
+            dataGridView1.ReadOnly = true;
+
+
+            MySqlCommand mComd;
+            MySqlDataReader reader;
+            mComd = new MySqlCommand("select * from place", ManageForm.mConn);
+            reader = mComd.ExecuteReader();
+
+            AutoCompleteStringCollection source = new AutoCompleteStringCollection();
+
+            while (reader.Read()) {
+                String tmp = reader["province"].ToString() + reader["city"].ToString() + reader["district"].ToString();
+                source.Add(tmp);
+                mp.Add(tmp, reader["place_id"].ToString());
+            }
+
+            reader.Dispose();
+            
+            textBox1.AutoCompleteCustomSource = source;
+            textBox1.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            textBox1.AutoCompleteSource = AutoCompleteSource.CustomSource;
+
+            textBox2.AutoCompleteCustomSource = source;
+            textBox2.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            textBox2.AutoCompleteSource = AutoCompleteSource.CustomSource;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String startAddr = textBox1.Text;
+            String endAddr = textBox2.Text;
+            if (!mp.ContainsKey(startAddr))
+            {
+                MessageBox.Show("出发地点不合法！");
+            }
+            else if (!mp.ContainsKey(endAddr))
+            {
+                MessageBox.Show("到达地点不合法！");
+            }
+            else
+            {
+                int tx = 0, ty = 0;
+                
+            }
         }
     }
 }
